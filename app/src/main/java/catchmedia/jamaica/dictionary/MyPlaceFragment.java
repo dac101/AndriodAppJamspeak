ce509d8994d0041adb26444b72f9961e88d7bdba
dac101 <dacor101@gmail.com>
2015-04-10 11:48:04 -0500

    Add more words to the database

--- app/src/main/java/catchmedia/jamaica/dictionary/MyPlaceFragment.java
+++ app/src/main/java/catchmedia/jamaica/dictionary/MyPlaceFragment.java
@@ -13,20 +13,28 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import org.json.JSONObject;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import database.DatabaseHandler;
 import database.Marker;
+import utility.GooglePlace;
+import utility.Helper;
 import utility.ImageInfo;
 
 
-public class MyPlaceFragment extends Fragment {
+public class MyPlaceFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-     JSONObject places;
+    JSONObject places;
+    GooglePlace place;
+    GoogleApiClient mGoogleApiClient;
+
     public MyPlaceFragment() {
         // Required empty public constructor
     }
@@ -42,43 +50,40 @@ public class MyPlaceFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.fragment_my_place, container, false);
         GridView gridView = (GridView) rootView.findViewById(R.id.grid_view);
 
-        getImages("travel");
-        //GooglePlaces place = new GooglePlaces();
+        Helper.getImages("travel", info);
 
-      //
-      // places = place.getValue(this.getActivity().getApplicationContext());
-
+        mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity().getApplicationContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+        place = new GooglePlace(mGoogleApiClient);
+        mGoogleApiClient.connect();
+        place.geolocationData();
+        places = place.getPlaces(this.getActivity().getApplicationContext());
 
 
         gridView.setAdapter(new MyAdapter(rootView.getContext(), info));
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View v,int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 List<Marker> markers;
                 DatabaseHandler db = new DatabaseHandler(rootView.getContext());
                 Marker mark = new Marker();
                 markers = db.getAllMarker();
 
-                for (Marker x : markers) {
-                    if (x.getName().contains(info.get(position).getName())) {
-                        mark = x;
+                for (Marker marker : markers) {
+                    if (marker.getName().contains(info.get(position).getName())) {
+                        mark = marker;
                         break;
+                    } else {
+                        mark = marker;
                     }
-                }
-                if (mark.getLatitude() == 0.0)
-                {
-                    mark= markers.get(1);
                 }
 
                 // Sending intent
                 Intent i = new Intent(getActivity(),
                         MapActivity.class);
                 // passing array index
-                i.putExtra("longitude", mark.getLongitude());
-                i.putExtra("latitude", mark.getLatitude());
-                i.putExtra("website", mark.getWebsite());
-                i.putExtra("number", mark.getNumber());
-                i.putExtra("email", mark.getEmail());
                 i.putExtra("name", mark.getName());
                 startActivity(i);
             }
@@ -90,6 +95,21 @@ public class MyPlaceFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        place.geolocationData();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 
     private class MyAdapter extends BaseAdapter {
@@ -154,29 +174,4 @@ public class MyPlaceFragment extends Fragment {
         }
     }
 
-    /**
-     * @param @dyamically gets all the images id and name put it
-     * @param @class      call ImageInfo
-     * @by using fields
-     */
-    public void getImages(String imageFilter) {
-        Field[] drawables = R.drawable.class.getFields();
-        ArrayList<Integer> ids = new ArrayList<Integer>();
-
-        int[] resArray = new int[drawables.length];
-
-        for (int i = 0; i < drawables.length; i++) {
-            try {
-                if (drawables[i].getName().contains(imageFilter)) {
-                    resArray[i] = drawables[i].getInt(null);
-                    ids.add(drawables[i].getInt(null));
-                    info.add(new ImageInfo(drawables[i].getName(), drawables[i].getInt(null)));
-                }
-            } catch (IllegalAccessException e) {
-                return;
-            } catch (IllegalArgumentException e) {
-                return;
-            }
-        }
-    }
 }
