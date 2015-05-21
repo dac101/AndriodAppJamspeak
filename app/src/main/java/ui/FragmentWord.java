455c3bf8ebe425af415c7c32fedfb4c759d4d746
dac101 <dacor101@gmail.com>
2015-05-21 18:58:57 -0500

    Refactor To make

---
+++ app/src/main/java/ui/FragmentWord.java
@@ -0,0 +1,72 @@
+package ui;
+
+import android.app.Activity;
+
+import android.content.res.AssetFileDescriptor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import adapter.Adapter;
+import catchmedia.jamaica.dictionary.R;
+import database.DatabaseHandler;
+import model.Word;
+
+public class FragmentWord extends Fragment {
+
+	private ListView listView;
+
+	Adapter listViewAdapter;
+
+	// database
+	DatabaseHandler db;
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+        ArrayList<Word>	words = (ArrayList<Word>) db.getAllWords();
+        Collections.sort(words, Word.WordNameComparator);
+
+		View rootView = inflater.inflate(R.layout.activity_fragment_word,container, false);
+		
+		listView = (ListView)rootView.findViewById(R.id.list_view);
+        listViewAdapter = new Adapter(getActivity().getBaseContext(),words);
+        listView.setAdapter(listViewAdapter);
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AssetFileDescriptor assetFileDescriptor = null;
+                try {
+                    assetFileDescriptor = getActivity().getAssets().openFd(listViewAdapter.getItem(position).getAudiofile().replace("&","").replace(" ",""));
+                    MediaPlayer player = new MediaPlayer();
+                    player.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
+                    player.prepare();
+                    player.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+		return rootView;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		db = new DatabaseHandler(activity);
+	}
+
+}
